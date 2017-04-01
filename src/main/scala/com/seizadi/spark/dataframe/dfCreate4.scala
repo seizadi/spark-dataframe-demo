bfb2a18bff62916744e651d751e472ba8f51ce5d
Soheil Eizadi <seizadi@infoblox.com>
2017-04-01 11:10:09 -0700

    Support for Redis

--- src/main/scala/com/seizadi/spark/dataframe/dfCreate4.scala
+++ src/main/scala/com/seizadi/spark/dataframe/dfCreate4.scala
@@ -1,6 +1,7 @@
 package com.seizadi.spark.dataframe
 
 import com.redislabs.provider.redis._
+import com.seizadi.spark.dataframe.dfCreate1.SampleSchema
 
 /**
   * Created by seizadi on 3/30/17.
@@ -20,16 +21,37 @@ object dfCreate4 extends App {
   import spark.implicits._
   import spark.sql
 
-  val redisConfig1 = new RedisConfig(new RedisEndpoint(redisHostname, redisPort, redisAuth))
-  val redisConfig2 = new RedisConfig(new RedisEndpoint("127.0.0.1", 7379))
+  // initial redis host - can be any node in cluster mode
+  spark.conf.set("redis.host", redisHostname)
 
-  val zsetRddFromEndpoint1 = {
-    implicit val c = redisConfig1
+  // initial redis port
+  spark.conf.set("redis.port", redisPort)
+
+  // optional redis AUTH password
+  spark.conf.set("redis.auth", redisAuth)
+
+  //val redisConfig1 = new RedisConfig(new RedisEndpoint(redisHostname, redisPort, redisAuth))
+  //val redisConfig2 = new RedisConfig(new RedisEndpoint("127.0.0.1", 7379))
+
+  val zSetRddFromEndPoint = spark.sparkContext.fromRedisZSetWithScore("portal.300050.month:client:class.count*")
+
+
+  //val zsetRddFromEndpoint1 = {
+   // implicit val c = redisConfig1
     // TODO: Parse all the keys for account
     // val zsetRDD = sc.fromRedisZSetWithScore("portal.300050.*")
-    spark.sparkContext.fromRedisZSetWithScore("portal.300050.month:client:class.count*")
-  }
+   // spark.sparkContext.fromRedisZSetWithScore("portal.300050.month:client:class.count*")
+  //}
 
-  zsetRddFromEndpoint1.toString
+  println("zset size " + zSetRddFromEndPoint.count())
+
+  case class RedisSchema(value: String, score: Double)
+
+  //val df = zSetRddFromEndPoint.map(value => value.split("\t")).toDF("value", "score")
+  // val rdd = zSetRddFromEndPoint.map(r => (r)).map(r => RedisSchema(r(0), r(1)))
+  zSetRddFromEndPoint.map(r => r).foreach(println)
+
+  //val df = rdd.toDF("value", "score")
+  //df.show()
 
 }
