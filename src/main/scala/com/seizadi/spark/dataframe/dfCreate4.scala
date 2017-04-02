a706ba8dafb65df2b118b3327e168b0293984cd9
Soheil Eizadi <seizadi@infoblox.com>
2017-04-02 10:13:32 -0700

    Checkpoint work on Redis

--- src/main/scala/com/seizadi/spark/dataframe/dfCreate4.scala
+++ src/main/scala/com/seizadi/spark/dataframe/dfCreate4.scala
@@ -2,6 +2,7 @@ package com.seizadi.spark.dataframe
 
 import com.redislabs.provider.redis._
 import com.seizadi.spark.dataframe.dfCreate1.SampleSchema
+import com.seizadi.spark.dataframe.dfCreate2.{rdd, schema, spark}
 
 /**
   * Created by seizadi on 3/30/17.
@@ -19,8 +20,8 @@ object dfCreate4 extends App {
   val spark = init.sparkSession
 
   import spark.implicits._
-  import spark.sql
 
+  // TODO: Support for cluster
   // initial redis host - can be any node in cluster mode
   spark.conf.set("redis.host", redisHostname)
 
@@ -30,28 +31,23 @@ object dfCreate4 extends App {
   // optional redis AUTH password
   spark.conf.set("redis.auth", redisAuth)
 
-  //val redisConfig1 = new RedisConfig(new RedisEndpoint(redisHostname, redisPort, redisAuth))
-  //val redisConfig2 = new RedisConfig(new RedisEndpoint("127.0.0.1", 7379))
+  // TODO: Parse all the keys for account
+  // val zsetRDD = sc.fromRedisZSetWithScore("portal.300050.*")
+  val zsetRDD = spark.sparkContext.fromRedisZSetWithScore("portal.300050.month:client:class.count*")
 
-  val zSetRddFromEndPoint = spark.sparkContext.fromRedisZSetWithScore("portal.300050.month:client:class.count*")
+  println("zset size " + zsetRDD.count())
 
+  val rdd = zsetRDD.
+    map(r => {
+      val v = r._1.split("\t")
+      val t = v(1).split("\n")
+      println("values are " + v(0) + " , " + t(0) + " , " + t(1) + " , " + t(2) + " , " + r._2)
+      (v(0), t(0), t(1), r._2)
+    })
+// }).toDF("client", "class", "count", "time")
+  println("rdd size " + rdd.count())
 
-  //val zsetRddFromEndpoint1 = {
-   // implicit val c = redisConfig1
-    // TODO: Parse all the keys for account
-    // val zsetRDD = sc.fromRedisZSetWithScore("portal.300050.*")
-   // spark.sparkContext.fromRedisZSetWithScore("portal.300050.month:client:class.count*")
-  //}
-
-  println("zset size " + zSetRddFromEndPoint.count())
-
-  case class RedisSchema(value: String, score: Double)
-
-  //val df = zSetRddFromEndPoint.map(value => value.split("\t")).toDF("value", "score")
-  // val rdd = zSetRddFromEndPoint.map(r => (r)).map(r => RedisSchema(r(0), r(1)))
-  zSetRddFromEndPoint.map(r => r).foreach(println)
-
-  //val df = rdd.toDF("value", "score")
-  //df.show()
+  val df = zsetRDD.toDF("value", "score")
+  df.show()
 
 }
